0bb79e86891658302b5c544fcaaaa83c5b662236
personmakesthings <jungleofhouseplants@outlook.com>
2024-07-17 15:29:03 +0100

    back button

--- src/app/please-login/page.jsx
+++ src/app/please-login/page.jsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import { LoginButton } from "@/components/LoginButton";
+import BackBtn from "@/components/BackBtn";
 
 export default async function PleaseLogin() {
   const session = await auth();
@@ -8,7 +9,8 @@ export default async function PleaseLogin() {
   if (!session) {
     return (
       <div className="max-w-screen-lg mx-auto p-4 mt-10">
-        You must login to vote on posts <LoginButton />
+        <p>You must login to vote on posts <LoginButton /></p>
+        <BackBtn />
       </div>
     )
   } else {
---
+++ src/components/BackBtn.jsx
@@ -0,0 +1,10 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
+export default function BackBtn() {
+    const router = useRouter()
+    return (
+        <p onClick={() => router.back} className="cursor-pointer"> ◄ Return to previous page</p>
+    )
+}
