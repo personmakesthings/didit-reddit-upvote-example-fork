5b9dba0b6bd3af273c538af9002a58ff5901c6df
personmakesthings <jungleofhouseplants@outlook.com>
2024-07-17 15:32:29 +0100

    back button

--- src/components/BackBtn.jsx
+++ src/components/BackBtn.jsx
@@ -5,6 +5,6 @@ import { useRouter } from "next/navigation"
 export default function BackBtn() {
     const router = useRouter()
     return (
-        <p onClick={() => router.back} className="cursor-pointer"> ◄ Return to previous page</p>
+        <p onClick={() => router.back()} className="cursor-pointer"> ◄ Return to previous page</p>
     )
 }
